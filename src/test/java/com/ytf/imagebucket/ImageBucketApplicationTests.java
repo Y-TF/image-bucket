ec0d807bb92fcda4788c3a58d387067d3de13c1e
23Yong <1239824@naver.com>
2022-12-22 22:12:44 +0900

    feat: 프로젝트 환결설정을 세팅

---
+++ src/test/java/com/ytf/imagebucket/ImageBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytf.imagebucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageBucketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
